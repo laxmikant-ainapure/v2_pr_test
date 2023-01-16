28f4c7e6a7dce120981d389ac2625f9d2512c877
Laxmikant Ainapure <76990221+laxmikant-ainapure@users.noreply.github.com>
2023-01-16 20:05:48 +0530

    Revert "Delete Category.java" This reverts commit 225aee694d7435dfd2267afbcbf3dbb9a0c552d8.

---
+++ DemoSubproject2.java
@@ -0,0 +1,51 @@
+package subproject2;
+
+import org.subproject2_A.App;
+
+public class DemoSubproject2 { 
+
+	int k;
+	
+	public static void main(String[] args) {
+		int k_subproject;
+	}
+	
+	private static DemoSubproject2 fook = null;
+
+	public static DemoSubproject2 getFook() { 
+		 System.out.println(""); 
+		 
+		if (fook==null) {
+			
+		
+			fook = new DemoSubproject2();
+		}              
+		System.out.println("therasdasde there");
+
+	int s1 = 58;  
+System.out.println("hello there there");
+		
+		return fook;
+		
+	}
+
+	
+	
+	public static int getIntValue() {
+	System.out.println("there there");
+	int k = 58; 
+	return k;	
+	}
+	
+	public void x(){
+		System.out.println("text");
+		try{
+			
+		}
+		catch(Exception ex){
+			
+		}
+		
+	}
+	
+}
